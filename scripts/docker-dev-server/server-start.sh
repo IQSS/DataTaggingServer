fb43700bcf977890fe92b5efbfdf5c49e95bfb2b
shadyObeed <48647554+shadyObeed@users.noreply.github.com>
2022-06-20 22:04:47 +0300

    Update server-start.sh

--- scripts/docker-dev-server/server-start.sh
+++ scripts/docker-dev-server/server-start.sh
@@ -1,3 +1,21 @@
 echo PolicyModelsServer dev env
 service postgresql start
 /bin/bash
+
+#clone the server repo:
+cd /var/pomos
+git clone https://github.com/IQSS/DataTaggingServer.git
+cd /var/pomos/DataTaggingServer/datatags-app
+
+#adding preconditions folders:
+mkdir -p var/models
+mkdir -p var/model-uploads
+
+#run the server in play mode
+sbt -Dplay.evolutions.db.default.autoApply=true -Dsbt.coursier.home="/var/coursier" run &
+
+#maybe it give it more sleep time depending on the server startup time
+sleep 240 &&
+
+#adding admin locally from the docker
+curl -X POST -d '{"username": "admin", "password":"pass"}' http://localhost:9000/admin/api/users/
